5074bc9f93ff0b6d3364eae0cdf93a08c9f26d83
microbit-carlos <carlos@microbit.org>
2019-02-07 12:01:24 +0000

    package: Remove jest watch plugins.

--- config/jest.config.js
+++ config/jest.config.js
@@ -18,10 +18,6 @@ const config = {
     },
   },
   setupFiles: ['<rootDir>/config/setup-tests.js'],
-  watchPlugins: [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
-  ],
 };
 
 module.exports = config;
