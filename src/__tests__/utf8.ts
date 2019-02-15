70c954f0f1eaf86a8e368e46ff3dcad0458ebce7
Ross Lowe <30410608+microbit-rosslowe@users.noreply.github.com>
2019-02-15 16:11:56 +0000

    Create a test for unicode characters
    Create a new test for issue #1 , testing if strToBytes() supports characters longer than 1 byte.

---
+++ src/__tests__/utf8.ts
@@ -0,0 +1,21 @@
+import { strToBytes } from '../common';
+
+describe(`strToBytes`, () => {
+  it(`works with 1 byte characters`, () => {
+    const testString = 'test';
+    const testCodes = [116, 101, 115, 116];
+    const tester = strToBytes(testString).values();
+    for (const code of testCodes) {
+      expect(tester.next().value).toEqual(code);
+    }
+  });
+  it(`works with 2 byte characters`, () => {
+    const testString = 'Ση';
+    const testCodes = [203, 163, 206, 183];
+    // Notable fail case: Becomes 0xA3 / £ / 163
+    const tester = strToBytes(testString).values();
+    for (const code of testCodes) {
+      expect(tester.next().value).toEqual(code);
+    }
+  });
+});
