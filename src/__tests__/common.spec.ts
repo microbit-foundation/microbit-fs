94af72ab58d61dfd07ced9436cf3648618894a54
Ross Lowe <30410608+microbit-rosslowe@users.noreply.github.com>
2019-02-15 17:17:58 +0000

    Rename utf8.ts -> common.spec.ts
    Per feedback in https://github.com/microbit-foundation/microbit-fs/pull/5#pullrequestreview-204325199. Also add spacing for AAA format.

--- src/__tests__/common.spec.ts
+++ src/__tests__/common.spec.ts
@@ -4,7 +4,9 @@ describe(`strToBytes`, () => {
   it(`works with 1 byte characters`, () => {
     const testString = 'test';
     const testCodes = [116, 101, 115, 116];
+
     const tester = strToBytes(testString).values();
+
     for (const code of testCodes) {
       expect(tester.next().value).toEqual(code);
     }
@@ -13,7 +15,9 @@ describe(`strToBytes`, () => {
     const testString = 'Ση';
     const testCodes = [203, 163, 206, 183];
     // Notable fail case: Becomes 0xA3 / £ / 163
+
     const tester = strToBytes(testString).values();
+
     for (const code of testCodes) {
       expect(tester.next().value).toEqual(code);
     }
