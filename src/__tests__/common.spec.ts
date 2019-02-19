e906516469fa8d2eebd65d272889128526e2c3c2
Ross Lowe <30410608+microbit-rosslowe@users.noreply.github.com>
2019-02-19 16:51:22 +0000

    Fix encoding for >1 byte characters
    - Properly encode >1 byte characters, closes #1 (adds text-encoding as a dependency)
    - Add test for bytesToStr()
    - Fix test for strToBytes(); add 3 byte character test

--- src/__tests__/common.spec.ts
+++ src/__tests__/common.spec.ts
@@ -1,4 +1,4 @@
-import { strToBytes } from '../common';
+import { bytesToStr, strToBytes } from '../common';
 
 describe(`strToBytes`, () => {
   it(`works with 1 byte characters`, () => {
@@ -13,13 +13,40 @@ describe(`strToBytes`, () => {
   });
   it(`works with 2 byte characters`, () => {
     const testString = 'Ση';
-    const testCodes = [203, 163, 206, 183];
-    // Notable fail case: Becomes 0xA3 / £ / 163
+    const testCodes = [206, 163, 206, 183];
 
     const tester = strToBytes(testString).values();
 
     for (const code of testCodes) {
       expect(tester.next().value).toEqual(code);
     }
+  });
+  it(`works with 3 byte characters`, () => {
+    const testString = '世';
+    const testCodes = [228, 184, 150];
+
+    const tester = strToBytes(testString).values();
+
+    for (const code of testCodes) {
+      expect(tester.next().value).toEqual(code);
+    }
+  });
+});
+
+describe(`bytesToStr`, () => {
+  it(`works with 1 byte characters`, () => {
+    const testCodes: Uint8Array = new Uint8Array([116, 101, 115, 116]);
+
+    expect(bytesToStr(testCodes)).toEqual('test');
+  });
+  it(`works with 2 byte characters`, () => {
+    const testCodes: Uint8Array = new Uint8Array([206, 163, 206, 183]);
+
+    expect(bytesToStr(testCodes)).toEqual('Ση');
+  });
+  it(`works with 3 byte characters`, () => {
+    const testCodes: Uint8Array = new Uint8Array([228, 184, 150]);
+
+    expect(bytesToStr(testCodes)).toEqual('世');
   });
 });
